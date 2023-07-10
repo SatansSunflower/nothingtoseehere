7d96fa35975147c8fbca94801048fbabc80ea3a4
Aida Begic <aida.begic@salzburg-ag.at>
2023-07-10 15:11:19 +0200

    comments

--- includes/db.php
+++ includes/db.php
@@ -1,13 +1,21 @@
 <?php
+/** 
+ * This is a common-module PHP file for connecting to a MySQL database using mysqli
+ */
 
+// These global constants define the connection information needed for the database 
 define ( 'DB_HOST', 'localhost' );
 define ( 'DB_USER', 'root' );
 define ( 'DB_PASSWORD', '' );
 define ( 'DB_NAME', 'php_lap_test' );
 
+/** 
+ * This creates a new connection to the MySQL database with the constants defined above. 
+ * The conn variable can then be used globally, provided that you include the db.php file
+**/
+ $conn = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
 
-$conn = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
-
+// If the connection cannot be established, then this error will be displayed on the page
 if ($conn->connect_errno) {
     die("ERROR: Unable to connect to database. " . $mysqli->connect_error);
 }
--- includes/functions.php
+++ includes/functions.php
@@ -1,7 +1,21 @@
 <?php
+/**
+ * This PHP file is used for common functions in regards to the database.
+ * You can find all sorts of needed functionalities for getting and manipulating data as well as displaying it. 
+ */
 
+// This imports the db.php file, which allows us to use the variables defined there, such as the connection to the database.
 require_once("db.php");
 
+/**
+ * This function returns one singular row from the provided table with the id.
+ * The function then renders the result and displays it in table form by calling the render_table() function.
+ * 
+ * @param string $table_name The name of the table you want to query.
+ * @param int $row_id The id of the row you want to recieve. 
+ * 
+ * @see render_table()
+ */
 function fetch_one_row($table_name, $row_id)
 {
     global $conn;
@@ -15,6 +29,15 @@ function fetch_one_row($table_name, $row_id)
     render_table($result);
 }
 
+
+/**
+ * This function returns every entry from the provided table.
+ * The function then renders the result and displays it in table form by calling the render_table() function.
+ * 
+ * @param string $table_name The name of the table you want to query.
+ * 
+ * @see render_table()
+ */
 function get_all_data_from_table($table_name)
 {
     global $conn;
@@ -27,6 +50,18 @@ function get_all_data_from_table($table_name)
     render_table($result);
 }
 
+
+/**
+ * This function takes values and the name of a table, then inserts the values into said table.
+ * Once created the function calls another one, which gets all the data again including the newly created row. 
+ * 
+ * @param string $table_name The name of the table you want to query.
+ * @param string $value_column1 The first value inserted into the table.
+ * @param string $value_column2 The second value inserted into the table.
+ * @param string $value_column3 The third value inserted into the table.
+ * 
+ * @see get_all_data_from_table()
+ */
 function insert_into_table($table_name, $value_column1, $value_column2, $value_column3)
 {
     global $conn;
@@ -39,9 +74,19 @@ function insert_into_table($table_name, $value_column1, $value_column2, $value_c
     $v3 = $value_column3;
     $stmt->execute();
 
-    echo "New rows created successfully.";
+    get_all_data_from_table($table_name);
 }
 
+/**
+ * This function takes a filter value and filters the database with the provided information.
+ * Once created the function calls another one, which renders all filtered data inside a table. 
+ * 
+ * @param string $table_name The name of the table you want to query.
+ * @param string $filter The value with which to filter.
+ * @param string $column_to_filter The column which should be filtered.
+ * 
+ * @see render_table()
+ */
 function filter_all_data($table_name, $filter, $column_to_filter)
 {
     global $conn;
@@ -56,23 +101,28 @@ function filter_all_data($table_name, $filter, $column_to_filter)
     render_table($result);
 }
 
+// TODO:
 function update_data_by_id($table_name, $column_to_update, $new_value, $row_id)
 {
     global $conn;
     $sql = "UPDATE {$table_name} SET {$column_to_update} = ? WHERE id = ?";
 
     $stmt = $conn->prepare($sql);
-    $stmt->bind_param("s", $new_value);
-    $stmt->bind_param("i", $row_id);
+    $stmt->bind_param("s", $new_val);
+    $stmt->bind_param("i", $id);
+
+    $new_val = $new_value;
+    $id = $row_id;
 
     if ($stmt->execute() === TRUE) {
         echo "Record updated successfully.";
-        // TODO: display this to the user in some way... 
+        fetch_one_row($table_name, $row_id);
     } else {
         echo "Error updating record: " . $conn->error;
     }
 }
 
+// TODO:
 function fetch_certain_data_rows($table_name, $amount_of_rows, $start_row = 0)
 {
     global $conn;
@@ -89,6 +139,7 @@ function fetch_certain_data_rows($table_name, $amount_of_rows, $start_row = 0)
     render_table($result);
 }
 
+// TODO:
 function delete_row_from_table($table_name, $row_id)
 {
     global $conn;
@@ -104,6 +155,7 @@ function delete_row_from_table($table_name, $row_id)
     }
 }
 
+// TODO:
 function search_data_by_input($table_name, $search_input)
 {
     global $conn;
@@ -120,6 +172,7 @@ function search_data_by_input($table_name, $search_input)
     render_table($result);
 }
 
+// TODO:
 function render_table($result)
 {
     if ($result->num_rows > 0) {
